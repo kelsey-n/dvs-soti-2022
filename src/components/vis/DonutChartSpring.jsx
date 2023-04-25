6b75a5fd08bd0206741c95e723c2263f5b5c649d
kelsey-n <kan317@nyu.edu>
2023-04-25 08:16:34 -0400

    opacity change on hover

--- src/components/vis/DonutChartSpring.jsx
+++ src/components/vis/DonutChartSpring.jsx
@@ -40,16 +40,6 @@ function DonutChartSpring({
 }) {
   const ref = useRef();
 
-  //   const arcGenerator = arc()
-  //     .innerRadius(innerRadiusScale(totalToolUsage[year]))
-  //     .outerRadius(function (d) {
-  //       return (
-  //         innerRadiusScale(totalToolUsage[year]) +
-  //         outerRadiusScale(d.data[`${year}_meantools`])
-  //       );
-  //     })
-  //     .cornerRadius(3);
-
   // Default sorting of the pie layout is by value descending (number of users of each tool)
   const pieGenerator = pie().value(function (d) {
     return d[`${year}_users`];
@@ -63,7 +53,6 @@ function DonutChartSpring({
     });
   }
   const pieData = useMemo(() => {
-    console.log('recalculating pie data');
     return pieGenerator(data);
   }, [data]); // TESTING: save pie data here to prevent recalculating it in hovering UE - this seems 'stickier' than recalculating pieGenerator(data) in the hovering UE for tt positioning
 
@@ -72,11 +61,17 @@ function DonutChartSpring({
     .domain(data.map((d) => d.tool))
     .range(schemeSet3);
 
-  // Tooltip
-  const tooltipWidth = 150;
-  let tooltipPos;
+  //   // Tooltip
+  //   const tooltipWidth = 150;
+  //   let tooltipPos;
+  //   const calculateTooltipPos = () => {
+  //     const hoveredData = //pieData.filter(
+  //       pieData.filter((d) => d.data.tool === hoveredTool)[0];
+  //     return arcGenerator.centroid(hoveredData);
+  //   };
+  //   tooltipPos = hoveredTool ? calculateTooltipPos() : [0, 0];
 
-  console.log(pieData);
+  //   console.log(pieData);
 
   // Here we will map over the pieData and return a Slice for each row of data
   const allSlices = pieData.map((sliceData, i) => {
@@ -87,6 +82,11 @@ function DonutChartSpring({
         year={year}
         color={color(sliceData.data.tool)}
         innerRadiusScale={innerRadiusScale}
+        outerRadiusScale={outerRadiusScale}
+        hoveredTool={hoveredTool}
+        setHoveredTool={setHoveredTool}
+        setShowTooltip={setShowTooltip}
+        setHoveredData={setHoveredData}
       />
     );
   });
@@ -99,35 +99,78 @@ function DonutChartSpring({
 }
 
 // Then we will create another component that returns a Slice component (eventually, an animated Slice)
-const Slice = ({ sliceData, year, color, innerRadiusScale }) => {
+const Slice = ({
+  sliceData,
+  year,
+  color,
+  innerRadiusScale,
+  outerRadiusScale,
+  hoveredTool,
+  setHoveredTool,
+  setShowTooltip,
+  setHoveredData,
+}) => {
   const arcGenerator = arc()
     .innerRadius(innerRadiusScale(totalToolUsage[year]))
-    // .outerRadius(function (d) {
-    //   return (
-    //     innerRadiusScale(totalToolUsage[year]) +
-    //     outerRadiusScale(d.data[`${year}_meantools`])
-    //   );
-    // })
-    .outerRadius(innerRadiusScale(totalToolUsage[year]) + 20)
+    .outerRadius(function (d) {
+      return (
+        innerRadiusScale(totalToolUsage[year]) +
+        outerRadiusScale(sliceData.data[`${year}_meantools`])
+      );
+    })
     .cornerRadius(3);
 
   const springProps = useSpring({
+    config: {
+      //   duration: 2000,
+    },
     to: {
       pos: [sliceData.startAngle, sliceData.endAngle],
     },
   });
 
+  //   // Tooltip
+  //   const tooltipWidth = 150;
+  //   let tooltipPos;
+  //   const calculateTooltipPos = () => {
+  //     const hoveredData = //pieData.filter(
+  //       pieData.filter((d) => d.data.tool === hoveredTool)[0];
+  //     return arcGenerator.centroid(hoveredData);
+  //   };
+  //   tooltipPos = hoveredTool ? calculateTooltipPos() : [0, 0];
+
+  const handleMouseOver = () => {
+    setHoveredTool(sliceData.data.tool);
+    setShowTooltip(true);
+    setHoveredData(sliceData);
+  };
+  const handleMouseOut = () => {
+    setHoveredTool(null);
+    setShowTooltip(false);
+    selectAll('.tooltip').remove();
+  };
+
   return (
     <animated.path
       d={springProps.pos.to((start, end) => {
         return arcGenerator({
-          //   innerRadius: 40,
-          //   outerRadius: radius,
           startAngle: start,
           endAngle: end,
         });
       })}
       fill={color}
+      style={{
+        opacity:
+          hoveredTool === null
+            ? 0.7
+            : sliceData.data.tool === hoveredTool
+            ? 1
+            : 0.3,
+        stroke: 'black',
+        strokeWidth: '0.5px',
+      }}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
     />
   );
 };
